54cfcd203436c7cc32a13ae375f43a9c1e93b924
kamrankhan0001 <kamranbgp15@gmail.com>
2025-06-18 15:08:40 +0530

    change4

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FaWhatsapp } from 'react-icons/fa'; 
-import flag from '../assets/Logos/Flag_of_India.png';
+import flag from '../assets/Logos/flag.png';
 
 
 
